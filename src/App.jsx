d26e32883b25d1b3b1c6bd7e2000fffa470b39c7
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-02-07 15:48:16 -0500

    useCallback Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -59,27 +59,29 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter(
-        (place) => place.id !== selectedPlace.current
-      )
-    );
-    setModalIsOpen(false);
+  const handleRemovePlace = useCallback(
+    function handleRemovePlace() {
+      setPickedPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current
+        )
+      );
+      setModalIsOpen(false);
 
-    const storedIds =
-      JSON.parse(localStorage.getItem("selectedPlaces")) || [];
-    localStorage.setItem(
-      "selectedPlaces",
-      JSON.stringify(
-        storedIds.filter((id) => id !== selectedPlace.current)
-      )
-    );
-  }
+      const storedIds =
+        JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify(
+          storedIds.filter((id) => id !== selectedPlace.current)
+        )
+      );
+    }, [])
+
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ children, open }) {
+function Modal({ children, open, onClose }) {
   const dialog = useRef();
 
 
@@ -16,8 +16,8 @@ function Modal({ children, open }) {
 
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
